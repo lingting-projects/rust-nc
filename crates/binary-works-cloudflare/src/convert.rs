9b4d41351ae89c18ecd150c3312365ef47e5c692
lingting <sunlisten.gzm@foxmail.com>
2025-11-05 16:04:19 +0800

    :zap: 转换时分享链接识别前置

--- crates/binary-works-cloudflare/src/convert.rs
+++ crates/binary-works-cloudflare/src/convert.rs
@@ -68,17 +68,36 @@ impl ConvertParams {
         Some(first.clone())
     }
 
-    pub fn from_fetch(req: Request) -> AnyResult<Self> {
-        Self::from_map(req.query_map()?)
+    pub fn from_fetch(req: Request, env: Env) -> AnyResult<Self> {
+        Self::from_map(req.query_map()?, env)
     }
 
-    pub fn from_map(source: HashMap<String, Vec<String>>) -> AnyResult<Self> {
+    pub fn from_map(source: HashMap<String, Vec<String>>, env: Env) -> AnyResult<Self> {
         let remote = Self::first(&source, "remote").expect_throw("remote not found");
         let tun = Self::convert_bool(source.get("tun")).unwrap_or(true);
         let ipv6 = Self::convert_bool(source.get("ipv6")).unwrap_or(true);
         let fake_ip = Self::convert_bool(source.get("fake_ip")).unwrap_or(true);
         let debug = Self::convert_bool(source.get("debug")).unwrap_or(false);
         let geo_cn = Self::convert_bool(source.get("geo_cn")).unwrap_or(true);
+
+        let uo = if remote.starts_with("s:") {
+            let source = &remote[2..];
+            console_debug!("分享源: {}", source);
+            let segments: Vec<&str> = source.split('?').collect();
+            let key = segments[0];
+            let p = if segments.len() > 1 {
+                segments[1].to_string()
+            } else {
+                "".to_string()
+            };
+            share::find_url(env, key, Some(&p))
+        } else {
+            Some(remote)
+        };
+        if uo == None {
+            return Err(Box::new(BizError::SubscribeNotFound));
+        }
+        let url = &uo.unwrap();
 
         let only_main = Self::convert_bool(source.get("include.only_main")).unwrap_or(false);
         let include: NodeContains = if only_main {
@@ -93,7 +112,7 @@ impl ConvertParams {
         }
 
         Ok(Self {
-            remote,
+            remote: url.clone(),
             tun,
             ipv6,
             fake_ip,
@@ -130,33 +149,14 @@ impl ConvertParams {
     }
 }
 
-async fn subscribe(env: Env, params: &ConvertParams) -> AnyResult<Subscribe> {
+async fn subscribe(params: &ConvertParams) -> AnyResult<Subscribe> {
     let remote = &params.remote;
-    console_debug!("从远程[{}]获取数据", &params.remote);
-    let uo = if remote.starts_with("s:") {
-        let source = &remote[2..];
-        console_debug!("分享源: {}", source);
-        let segments: Vec<&str> = source.split('?').collect();
-        let key = segments[0];
-        let p = if segments.len() > 1 {
-            segments[1].to_string()
-        } else {
-            "".to_string()
-        };
-        share::find_url(env, key, Some(&p))
-    } else {
-        Some(remote.clone())
-    };
-    if uo == None {
-        return Err(Box::new(BizError::SubscribeNotFound))
-    }
-    let url = &uo.unwrap();
-
-    let mut response = http_get(url).await?;
+    console_debug!("从远程[{}]获取数据", remote);
+    let mut response = http_get(remote).await?;
     if response.status_code() != 200 {
         return Err(Box::new(RustError(format!(
             "远程[{}]返回异常! {}",
-            url,
+            remote,
             response.status_code()
         ))));
     }
@@ -184,11 +184,11 @@ async fn build_remote(
     rules_reject: Vec<Rule>,
 ) -> AnyResult<Remote> {
     console_debug!("解析参数");
-    let params = ConvertParams::from_fetch(req)?;
+    let params = ConvertParams::from_fetch(req, env)?;
     console_debug!("解析远程地址: {}", &params.remote);
     let host = pick_host(&params.remote).expect_throw("invalid remote");
     console_debug!("远程域名: {}", &host);
-    let subscribe = subscribe(env, &params).await?;
+    let subscribe = subscribe(&params).await?;
     let info = subscribe.info();
     console_debug!("构造配置");
     let config = params.build_config(subscribe, rules_direct, rules_proxy, rules_reject)?;
